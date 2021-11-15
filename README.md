254f3bbd05f07653235ef6932619279a81912060
ŞERİF ÇOLAKEL <84101614+serifcolakel@users.noreply.github.com>
2021-11-15 21:12:30 +0300

    course database'e eklendi

---
+++ README.md
@@ -0,0 +1,12 @@
+# KURULAN PAKETLER 
+* npm i mongoose 
+*
+
+# Shema Types 
+>* String  
+>* Number
+>* Date
+>* Buffer(ikilik taban için)
+>* Boolean 
+>* Array
+>* ObjectID
---
+++ index.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/playground", { useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
+
+// Schema Types : String, Number, Date, Buffer, Boolean, Mixed, Objectid, Array
+const coursesSchema = new mongoose.Schema({
+  name: String,
+  author: String, // türü string
+  tags: [String], // Array of strings
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
+
+// Course sınıfı içinde NodeJs objesi oluşturacaz model ile
+const Course = mongoose.model("Course", coursesSchema);
+
+async function createCourse() {
+  const course = new Course({
+    name: "NodeJs",
+    author: "Mehmet",
+    tags: ["node", "back-end"],
+    date: new Date(),
+    isPublished: true,
+  });
+  const result = await course.save();
+  console.log(result);
+}
+
+createCourse();
