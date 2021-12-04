d0272a1522dad30bd1c8a4e332884ea231e68374
ŞERİF ÇOLAKEL <84101614+serifcolakel@users.noreply.github.com>
2021-12-04 18:13:53 +0300

    Login/auth endpointi eklendi ve test edildi

--- vidlyAPI/index.js
+++ vidlyAPI/index.js
@@ -6,11 +6,19 @@ const customers = require("./routes/customers");
 const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
 const users = require("./routes/users");
+const auth = require("./routes/auth");
 const express = require("express");
 const app = express();
-
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
 mongoose
-  .connect("mongodb://localhost/vidly", { useNewUrlParser: true })
+  .connect("mongodb://localhost/vidly", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB..."));
 
@@ -20,6 +28,7 @@ app.use("/api/customers", customers);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
 app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ vidlyAPI/routes/auth.js
@@ -0,0 +1,37 @@
+const Joi = require("joi");
+const bcrypt = require("bcrypt");
+const _ = require("lodash");
+const { User } = require("../models/user");
+const mongoose = require("mongoose");
+const express = require("express");
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  console.log(error);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+
+  if (!user) return res.status(400).send("Invalid email or password.");
+
+  // req.body.password ile user.password'u karşılaştıracak
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  console.log("body-password:", req.body.password);
+  console.log("user-password:", user.password);
+  console.log("compare result:", validPassword);
+  if (!validPassword) return res.status(400).send("Invalid email or password.");
+
+  res.send(true);
+});
+
+function validate(req) {
+  const schema = {
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required(),
+  };
+
+  return Joi.validate(req, schema);
+}
+
+module.exports = router;
